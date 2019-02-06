fa78cf6567c01a7669904a2891bbb8d2e0245a21
xavier-castro <xavier_castro@icloud.com>
2019-02-05 21:44:34 -0600

    Refactored code to use less global variables

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,7 +12,6 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     var audioPlayer: AVAudioPlayer!
-    var selectedSoundFileName: String = ""
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
@@ -21,13 +20,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        selectedSoundFileName = soundArray[sender.tag - 1]
+        let selectedSoundFileName: String = soundArray[sender.tag - 1]
         
-        playSound()
+        playSound(soundFileName: selectedSoundFileName)
     }
     
-    func playSound() {
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+    func playSound(soundFileName: String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
