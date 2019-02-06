bf98975759ea624cda8526c111fe6a2593604e3c
xavier-castro <xavier_castro@icloud.com>
2019-02-05 20:42:46 -0600

    Added sound when button is tapped

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController {
+    var player: AVAudioPlayer?
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,9 +19,17 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
         
-        
-        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error as Error {
+            print(error)
+        }
     }
     
   
