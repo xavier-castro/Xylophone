c22152b4383b8028cbf7d8d2cb18bfd3230a5d2e
xavier-castro <xavier_castro@icloud.com>
2019-02-05 21:33:52 -0600

    Different sounds corresponds with different buttons

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -3,6 +3,7 @@
 //  Xylophone
 //
 //  Created by Angela Yu on 27/01/2016.
+//  Edited by Xavier Castro
 //  Copyright © 2016 London App Brewery. All rights reserved.
 //
 
@@ -11,6 +12,8 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     var audioPlayer: AVAudioPlayer!
+    var selectedSoundFileName: String = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +21,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        playSound()
+    }
+    
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
